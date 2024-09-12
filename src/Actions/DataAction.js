01077f4ab4d2390950d4e3ed61cb3b4f0ee6e7a2
Abhijeet SINGH <abhijeetsingh16666@gmail.com>
2024-09-13 02:06:19 +0530

    done

--- src/Actions/DataAction.js
+++ src/Actions/DataAction.js
@@ -54,7 +54,7 @@ export const selectData = (group, allTickets, orderValue) => async (dispatch) =>
                 })
             })
         }else{
-            let prior_list = ["No priority", "Low", "Medium", "High", "Urgent"];
+            let prior_list = ["No priority", "High", "Low", "Medium", "Urgent"];
 
             prior_list.forEach((elem, index) => {
                 arr = allTickets.filter((fElem) => {
